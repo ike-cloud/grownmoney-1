781655d83d619237b610c49f2c9dd233431acbbb
ike <2740262535@qq.com>
2020-10-30 13:38:08 +0800

    添加404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <div>您所输入的网址不正确</div>
+  <div><router-link to="/money">返回首页</router-link></div>
+
+</div>
+</template>
+
+<script lang="ts">
+export default {
+name: "NotFound"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
